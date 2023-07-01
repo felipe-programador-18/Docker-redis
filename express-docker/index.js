ecbc7cea54608f3533e97c2b69a1dc01add82383
felipe-programador-18 <machadofelipe2016@outlook.com>
2023-07-01 15:16:58 -0300

    finish my structure up

--- express-docker/index.js
+++ express-docker/index.js
@@ -4,8 +4,9 @@ const app = Express()
 
 app.get( "/", (req, res, next) => {
   res.send('connect my api here now')
-  next('I am here 3000 port')
   console.log('testing here now')
 })
 
-app.listen(3000)
+app.listen(8080 , () => {
+    console.log(`listing the port here more one time ${8080}`)
+})
